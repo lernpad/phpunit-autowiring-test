630e68f04001b0b59db5d07c7e5eadbecc771006
Andrey Chernov <lernpad@mail.ru>
2017-05-26 01:07:24 +0700

    init

---
+++ tests/FirstTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+
+    public function testHello()
+    {
+        $this->assertTrue(true);
+    }
+
+}
